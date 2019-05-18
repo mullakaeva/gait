4bba098eb75afb407a22ca9cf75ef92bf42692fb
yyhhoi <yyhhoi@gmail.com>
2019-05-18 07:13:08 +0200

    try out latent dimensions

--- scripts/TemporalVAE/TemporalVAE_run.py
+++ scripts/TemporalVAE/TemporalVAE_run.py
@@ -67,13 +67,13 @@ class GaitTVAEmodel:
                     self.model.eval()
                     with torch.no_grad():
                         out_test, mu_test, logvar_test, z_test = self.model(x_test)
-                        loss_test = self.loss_function(x_test, out_test, mu_test, logvar_test)
+                        loss_test, kld_loss_test = self.loss_function(x_test, out_test, mu_test, logvar_test)
                         self.loss_cv_meter.update(loss_test.item())
 
                     # Train set
                     self.model.train()
                     out, mu, logvar, z = self.model(x)
-                    loss = self.loss_function(x, out, mu, logvar)
+                    loss, kld_loss = self.loss_function(x, out, mu, logvar)
                     self.loss_train_meter.update(loss.item())
 
                     # Back-prop
@@ -82,11 +82,13 @@ class GaitTVAEmodel:
                     iter_idx += 1
 
                     # Print Info
-                    print("Epoch %d/%d at iter %d/%d | Loss: %0.4f | CV Loss: %0.4f" % (self.epoch, n_epochs, iter_idx,
-                                                                                        self.data_gen.num_rows / self.data_gen.m,
-                                                                                        self.loss_train_meter.avg,
-                                                                                        self.loss_cv_meter.avg)
-                          )
+                    print("\rEpoch %d/%d at iter %d/%d | Loss: %0.8f | CV Loss: %0.8f" % (self.epoch, n_epochs,
+                                                                                          iter_idx,
+                                                                                          self.data_gen.num_rows / self.data_gen.m,
+                                                                                          self.loss_train_meter.avg,
+                                                                                          self.loss_cv_meter.avg),
+                          flush=True, end="")
+                print()
                 # save (overwrite) model file every epoch
                 self._save_model()
                 self._plot_loss()
@@ -166,24 +168,16 @@ class GaitTVAEmodel:
         if self.kld is None:
             kld_loss = 0
         else:
-            kld_loss = self.kld * (-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()))
+            kld_loss = self.kld * (-0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp()))
         # Set recon loss
-        recon_loss = torch.sum(self.weights_vec * ((x - pred) ** 2))
+        recon_loss = torch.mean(self.weights_vec * ((x - pred) ** 2))
         # Combine different losses
         loss = recon_loss + kld_loss
-        return loss
+        return loss, kld_loss
 
     def _plot_loss(self):
-        epoch_windows = 50
-        self.loss_recorder["train"].append(self.loss_train_meter.avg)
-        self.loss_recorder["cv"].append(self.loss_cv_meter.avg)
-        self.epoch = len(self.loss_recorder["train"])
-        fig, ax = plt.subplots(2, 2, figsize=(12, 12))
-        ax[0, 0].plot(self.loss_recorder["train"], label="train")
-        ax[1, 0].plot(self.loss_recorder["cv"], label="cv")
 
-        # Restrict to show only recent epochs
-        if self.epoch > epoch_windows:
+        def sliding_plot(epoch_windows, ax1, ax2):
             x_length = np.linspace(self.epoch - epoch_windows, self.epoch - 1, epoch_windows)
             y_train = self.loss_recorder["train"][self.epoch - epoch_windows:]
             y_cv = self.loss_recorder["cv"][self.epoch - epoch_windows:]
@@ -191,11 +185,40 @@ class GaitTVAEmodel:
             y_min_train, y_max_train = np.min(y_train), np.max(y_train)
             y_min_cv, y_max_cv = np.min(y_cv), np.max(y_cv)
 
-            ax[0, 1].plot(x_length, y_train, label="train")
-            ax[1, 1].plot(x_length, y_cv, label="cv")
-            ax[0, 1].set_ylim(y_min_train, y_max_train)
-            ax[1, 1].set_ylim(y_min_cv, y_max_cv)
+            ax1.plot(x_length, y_train, label="train")
+            ax2.plot(x_length, y_cv, label="cv")
+            ax1.set_ylim(y_min_train, y_max_train)
+            ax2.set_ylim(y_min_cv, y_max_cv)
 
+        epoch_windows_original = 100
+        epoch_windows_zoomed = 20
+
+        self.loss_recorder["train"].append(self.loss_train_meter.avg)
+        self.loss_recorder["cv"].append(self.loss_cv_meter.avg)
+        self.epoch = len(self.loss_recorder["train"])
+        fig, ax = plt.subplots(2, 2, figsize=(12, 12))
+        # ax[0, 0].plot(self.loss_recorder["train"], label="train")
+        # ax[1, 0].plot(self.loss_recorder["cv"], label="cv")
+
+        # Restrict to show only recent epochs
+        if self.epoch > epoch_windows_original:
+            sliding_plot(epoch_windows_original, ax[0, 0], ax[1, 0])
+        else:
+            ax[0, 0].plot(self.loss_recorder["train"], label="train")
+            ax[1, 0].plot(self.loss_recorder["cv"], label="cv")
+            # x_length = np.linspace(self.epoch - epoch_windows, self.epoch - 1, epoch_windows)
+            # y_train = self.loss_recorder["train"][self.epoch - epoch_windows:]
+            # y_cv = self.loss_recorder["cv"][self.epoch - epoch_windows:]
+            #
+            # y_min_train, y_max_train = np.min(y_train), np.max(y_train)
+            # y_min_cv, y_max_cv = np.min(y_cv), np.max(y_cv)
+            #
+            # ax[0, 1].plot(x_length, y_train, label="train")
+            # ax[1, 1].plot(x_length, y_cv, label="cv")
+            # ax[0, 1].set_ylim(y_min_train, y_max_train)
+            # ax[1, 1].set_ylim(y_min_cv, y_max_cv)
+        if self.epoch > epoch_windows_zoomed:
+            sliding_plot(epoch_windows_zoomed, ax[0, 1], ax[1, 1])
         else:
             ax[0, 1].axis("off")
             ax[1, 1].axis("off")
@@ -281,7 +304,7 @@ class GaitCVAEvisualiser:
                                  pca_enableds=[True, False]):
 
         z, labels = self._get_all_latents_results()
-        pca_model = PCA(n_components=200)
+        pca_model = PCA(n_components=50)
         z_pca = pca_model.fit_transform(z)
         save_img_dir = os.path.join(self.save_vid_dir, "umap")
         # Loop for creating umap
@@ -289,12 +312,12 @@ class GaitCVAEvisualiser:
             for min_dist in min_dists:
                 for metric in metrics:
                     for pca in pca_enableds:
+                        umap_identifier = "PCA-{}_neigh-{}_dist-{}_metric-{}".format(pca, neigh, min_dist, metric)
 
                         save_img1 = os.path.join(save_img_dir,
                                                  self.model_identifier + "_type1_" + umap_identifier + ".png")
-                        save_img2 = os.path.join(
-                            save_img_dir, self.model_identifier + "_type2_" + umap_identifier + ".png")
-                        umap_identifier = "PCA-{}_neigh-{}_dist-{}_metric-{}".format(pca, neigh, min_dist, metric)
+                        save_img2 = os.path.join(save_img_dir,
+                                                 self.model_identifier + "_type2_" + umap_identifier + ".png")
                         print("Visualizing U-map | {}".format(umap_identifier))
 
                         # U-map embedding
@@ -314,7 +337,6 @@ class GaitCVAEvisualiser:
                         draw_clusters_sin = plot_latent_labels_cluster(embedding, labels, title, alpha=0.2)
                         plt.savefig(save_img2, dpi=300)
                         plt.close()
-
 
     def _get_pred_results(self):
         for (x_train, labels_train), (_, _) in self.data_gen.iterator():
