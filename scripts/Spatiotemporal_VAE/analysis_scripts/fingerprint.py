f0a94096999ba6954bd7d71fa6e318d8b9854232
yyhhoi <yyhhoi@gmail.com>
2019-07-31 20:53:37 +0200

    Dual fingerprint analysis finished

--- scripts/Spatiotemporal_VAE/analysis_scripts/fingerprint.py
+++ scripts/Spatiotemporal_VAE/analysis_scripts/fingerprint.py
@@ -2,23 +2,28 @@ import numpy as np
 import pandas as pd
 import os
 import umap
+import torch
 from common.data_preparation import prepare_data_for_concatenated_latent
-from common.utils import numpy2tensor, tensor2numpy, write_df_pickle
+from common.utils import numpy2tensor, tensor2numpy, write_df_pickle, expand1darr
 
 
 class FingerprintSaver:
     def __init__(self, model_container, load_df_path, save_data_dir, save_df_name):
-        print("Constructing dataframe of each patient's fingerprint.")
         self.model_container = model_container
-        self.df = self._load_dataframe(load_df_path)
+        self.load_df_path = load_df_path
         self.save_data_dir = save_data_dir
         self.save_df_name = save_df_name
 
-        # Reserve
+        # Reserved
+        self.df = None
         self.task_means = dict()
         self.df_concat = None
 
     def process_and_save(self):
+
+        # Load input dataframe
+        self.df = self._load_dataframe(self.load_df_path)
+
         # Forward pass and add column
         self._forward_pass()
 
@@ -36,28 +41,36 @@ class FingerprintSaver:
 
     def _load_dataframe(self, df_path):
         # Load data
+        print("Constructing dataframe of each patient's fingerprint.")
         df_processed = prepare_data_for_concatenated_latent(df_path, equal_phenos=False)
         df_shuffled = df_processed.sample(frac=1, random_state=60).reset_index(drop=True)
         del df_processed
         return df_shuffled
 
-    def _forward_pass(self):
+    def _define_model_input(self):
         x = np.asarray(list(self.df["features"]))
-
-        motion_z_list = []
-        batch = 512
-        batch_times = int(x.shape[0] / batch)
+        batch_size = 512
+        batch_times = int(x.shape[0] / batch_size)
         for i in range(batch_times + 1):
             if i < batch_times:
-                x_each = numpy2tensor(self.model_container.device, x[i * batch: (i + 1) * batch, ])[0]
+                x_each = numpy2tensor(self.model_container.device, x[i * batch_size: (i + 1) * batch_size, ])[0]
             else:
-                x_each = numpy2tensor(self.model_container.device, x[i * batch:, ])[0]
-            _, _, _, motion_z_batch = self.model_container._forward_pass(x_each)
+                x_each = numpy2tensor(self.model_container.device, x[i * batch_size:, ])[0]
+            yield (x_each,)
+
+    def _forward_pass(self):
+
+        # Batch-wise forward inference
+        motion_z_list = []
+        for model_inputs in self._define_model_input():
+            _, _, _, motion_z_batch = self.model_container._forward_pass(*model_inputs)
             motion_z_batch = tensor2numpy(motion_z_batch)[0]
             motion_z_list.append(motion_z_batch)
 
+        # Stack and add to dataframe
         motion_z = np.vstack(motion_z_list)
         self.df["motion_z"] = list(motion_z)
+
         # Averaging over split video rows and delete unnecessary columns
         del self.df["features"]  # The "features" column is not needed anymore after forward pass
         self.df = self.df.groupby("avg_idx", as_index=False).apply(
@@ -116,31 +129,109 @@ class FingerprintSaver:
         write_df_pickle(self.df_concat, os.path.join(self.save_data_dir,
                                                      self.save_df_name))
 
+
 class CondFingerprintSaver(FingerprintSaver):
 
-    def _forward_pass(self):
+    def _define_model_input(self):
 
+        # Extract data
         x = np.asarray(list(self.df["features"]))
         directions = np.asarray(list(self.df["directions"]))
 
-        motion_z_list = []
-        batch = 512
-        batch_times = int(x.shape[0] / batch)
+        # Expand dimension
+        directions = expand1darr(directions.astype(np.int64), 3, self.model_container.seq_dim)
+
+        # Generator
+        batch_size = 512
+        batch_times = int(x.shape[0] / batch_size)
         for i in range(batch_times + 1):
             if i < batch_times:
-                direction_each = directions[i * batch: (i + 1) * batch, ]
-                x_each, direction_each = numpy2tensor(self.model_container.device, x[i * batch: (i + 1) * batch, ],
-                                                      direction_each)
+                x_each, direction_each = numpy2tensor(self.model_container.device,
+                                                      x[i * batch_size: (i + 1) * batch_size, ],
+                                                      directions[i * batch_size: (i + 1) * batch_size, ])
             else:
-                direction_each = directions[i * batch:, ]
-                x_each, direction_each = numpy2tensor(self.model_container.device, x[i * batch:, ], direction_each)
+                x_each, direction_each = numpy2tensor(self.model_container.device,
+                                                      x[i * batch_size:, ],
+                                                      directions[i * batch_size:, ])
+            yield (x_each, direction_each)
 
-            _, _, _, motion_z_batch = self.model_container._forward_pass(x_each, direction_each)
-            motion_z_batch = tensor2numpy(motion_z_batch)[0]
-            motion_z_list.append(motion_z_batch)
 
-        motion_z = np.vstack(motion_z_list)
-        self.df["motion_z"] = list(motion_z)
+
+class DualDirectTaskFingerprintSaver(FingerprintSaver):
+
+    def __init__(self, model_container1, model_container2, load_df_path, save_data_dir, save_df_name):
+        """
+
+        Parameters
+        ----------
+        model_container1
+            CSTVAE_model instance
+        model_container2
+            CtaskSTVAE_model instance
+        load_df_path
+        save_data_dir
+        save_df_name
+        """
+
+        self.model_container1 = model_container1
+        self.model_container2 = model_container2
+        self.load_df_path = load_df_path
+        self.save_data_dir = save_data_dir
+        self.save_df_name = save_df_name
+
+        # Reserved
+        self.df = None
+        self.task_means = dict()
+        self.df_concat = None
+
+    def _define_model_input(self):
+
+        # Extract data
+        x = np.asarray(list(self.df["features"]))
+        directions = np.asarray(list(self.df["directions"]))
+        tasks = np.asarray(list(self.df["tasks"]))
+
+        # Expand dimension
+        directions = expand1darr(directions.astype(np.int64), 3, self.model_container1.seq_dim)
+        tasks = expand1darr(tasks.astype(np.int64), 8, self.model_container2.seq_dim)
+
+        # Generator
+        batch_size = 512
+        batch_times = int(x.shape[0] / batch_size)
+        for i in range(batch_times + 1):
+            if i < batch_times:
+                x_each, direction_each, task_each = numpy2tensor(self.model_container1.device,
+                                                                 x[i * batch_size: (i + 1) * batch_size, ],
+                                                                 directions[i * batch_size: (i + 1) * batch_size, ],
+                                                                 tasks[i * batch_size: (i + 1) * batch_size])
+            else:
+                x_each, direction_each, task_each = numpy2tensor(self.model_container1.device,
+                                                                 x[i * batch_size:, ],
+                                                                 directions[i * batch_size:, ],
+                                                                 tasks[i * batch_size:, ])
+            # Concatenate. "direction_each" for CSTVAE_model, "concated_cond" for CtaskSTVAE_model.
+            concated_cond = torch.cat((direction_each, task_each), dim=1)
+            yield (x_each, direction_each, concated_cond)
+
+    def _forward_pass(self):
+
+        # Batch-wise forward inference
+        motion_z_list1 = []
+        motion_z_list2 = []
+        for model_inputs in self._define_model_input():
+            _, _, _, motion_z_batch1 = self.model_container1._forward_pass(*model_inputs[0:2])
+            _, _, _, motion_z_batch2 = self.model_container2._forward_pass(*(model_inputs[0], model_inputs[2]))
+            motion_z_batch1 = tensor2numpy(motion_z_batch1)[0]
+            motion_z_batch2 = tensor2numpy(motion_z_batch2)[0]
+            motion_z_list1.append(motion_z_batch1)
+            motion_z_list2.append(motion_z_batch2)
+
+        # Stack and add to dataframe
+        motion_z1 = np.vstack(motion_z_list1)
+        motion_z2 = np.vstack(motion_z_list2)
+
+        self.df["motion_z"] = list(np.concatenate([motion_z1, motion_z2], axis=1))
+
         # Averaging over split video rows and delete unnecessary columns
         del self.df["features"]  # The "features" column is not needed anymore after forward pass
         self.df = self.df.groupby("avg_idx", as_index=False).apply(
--- scripts/spatiotemporal_vae_script.py
+++ scripts/spatiotemporal_vae_script.py
@@ -3,13 +3,11 @@
 from Spatiotemporal_VAE.STVAE_run import STVAEmodel, CSTVAEmodel, CtaskSVAEmodel, CtaskLegSVAEmodel
 from common.generator import GaitGeneratorFromDFforTemporalVAE
 from common.utils import dict2json, json2dict
-from Spatiotemporal_VAE.analysis_scripts.visualize_convex_direction import save_for_convex_direction
 from Spatiotemporal_VAE.analysis_scripts.latent_space_visualization import LatentSpaceSaver_CondDirectTask
 import os
 import pprint
 
-df_path = "/mnt/data/feas_tasks_phenos_nanMasks_idpatient_leg.pickle"
-concatenated_df_path = "/mnt/data/concatenated_latents.pickle"
+
 
 
 def print_model_info(model_identifier, hyper_params):
@@ -17,18 +15,12 @@ def print_model_info(model_identifier, hyper_params):
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(hyper_params)
 
-
-def run_train_and_vis_on_stvae():
+def load_model_container(model_identifier, df_path):
     # Hard-coded stuffs
     seq_dim = 128
     init_lr = 0.001
     lr_milestones = [75]
     lr_decay_gamma = 0.1
-
-    # Naming of models: N=Normal
-    # model_identifier = "Cond_Direct_Leg_K-0.0001"  # Direction + Leg
-    model_identifier = "Cond_Task_Direct_K-0.0001"  # Direction + Task
-    # model_identifier = "CB-K(0.0001)-C-G-S2-New"  # Only Direction
 
     # Hyper-parameters
     hyper_params = {
@@ -49,12 +41,7 @@ def run_train_and_vis_on_stvae():
         "latent_recon_loss": 1,
         "recon_loss_power": 2
     }
-
-    # Define paths
-    # df_path = "/mnt/data/raw_features_zmatrix_row_labels_withNanMasks.pickle"
-
     save_model_path = "Spatiotemporal_VAE/model_chkpt/ckpt_%s.pth" % model_identifier
-    project_dir = "Spatiotemporal_VAE"
     save_hyper_params_path = "Spatiotemporal_VAE/model_chkpt/hyperparms_%s.json" % model_identifier
 
     if os.path.isfile(save_model_path):
@@ -72,51 +59,81 @@ def run_train_and_vis_on_stvae():
 
     print_model_info(model_identifier, hyper_params)
 
-    # Train
     data_gen = GaitGeneratorFromDFforTemporalVAE(df_path, m=512, n=seq_dim)
 
     model_container = CtaskSVAEmodel(data_gen=data_gen, fea_dim=50, seq_dim=seq_dim,
-                                        conditional_label_dim=hyper_params["conditional_label_dim"],
-                                        model_type=hyper_params["model_type"],
-                                        posenet_latent_dim=hyper_params["posenet_latent_dim"],
-                                        posenet_dropout_p=hyper_params["posenet_dropout_p"],
-                                        posenet_kld=hyper_params["posenet_kld"],
-                                        motionnet_latent_dim=hyper_params["motionnet_latent_dim"],
-                                        motionnet_hidden_dim=512,
-                                        motionnet_dropout_p=hyper_params["motionnet_dropout_p"],
-                                        motionnet_kld=hyper_params["motionnet_kld"],
-                                        pose_latent_gradient=hyper_params["pose_latent_gradient"],
-                                        recon_gradient=hyper_params["recon_gradient"],
-                                        classification_weight=hyper_params["class_weight"],
-                                        rmse_weighting_startepoch=hyper_params["rmse_weighting_startepoch"],
-                                        latent_recon_loss=hyper_params["latent_recon_loss"],
-                                        init_lr=init_lr, lr_milestones=lr_milestones, lr_decay_gamma=lr_decay_gamma,
-                                        save_chkpt_path=save_model_path, load_chkpt_path=load_model_path)
-    # model_container._save_model()
+                                     conditional_label_dim=hyper_params["conditional_label_dim"],
+                                     model_type=hyper_params["model_type"],
+                                     posenet_latent_dim=hyper_params["posenet_latent_dim"],
+                                     posenet_dropout_p=hyper_params["posenet_dropout_p"],
+                                     posenet_kld=hyper_params["posenet_kld"],
+                                     motionnet_latent_dim=hyper_params["motionnet_latent_dim"],
+                                     motionnet_hidden_dim=512,
+                                     motionnet_dropout_p=hyper_params["motionnet_dropout_p"],
+                                     motionnet_kld=hyper_params["motionnet_kld"],
+                                     pose_latent_gradient=hyper_params["pose_latent_gradient"],
+                                     recon_gradient=hyper_params["recon_gradient"],
+                                     classification_weight=hyper_params["class_weight"],
+                                     rmse_weighting_startepoch=hyper_params["rmse_weighting_startepoch"],
+                                     latent_recon_loss=hyper_params["latent_recon_loss"],
+                                     init_lr=init_lr, lr_milestones=lr_milestones, lr_decay_gamma=lr_decay_gamma,
+                                     save_chkpt_path=save_model_path, load_chkpt_path=load_model_path)
+    return model_container, save_model_path
+
+def run_train_and_vis_on_stvae():
+
+    df_path = "/mnt/data/feas_tasks_phenos_nanMasks_idpatient_leg.pickle"
+    # model_identifier = "Cond_Direct_Leg_K-0.0001"  # Direction + Leg
+    model_identifier = "Cond_Task_Direct_K-0.0001"  # Direction + Task
+    # model_identifier = "CB-K(0.0001)-C-G-S2-New"  # Only Direction
+
+    model_container, save_model_path = load_model_container(model_identifier, df_path)
 
     # model_container.train(900)
-    #
+
     # Visualization
     if os.path.isfile(save_model_path):
-        data_gen2 = GaitGeneratorFromDFforTemporalVAE(df_path, m=data_gen.num_rows - 1, n=seq_dim, seed=60)
+        data_gen2 = GaitGeneratorFromDFforTemporalVAE(df_path,
+                                                      m=model_container.data_gen.num_rows - 1,
+                                                      n=model_container.seq_dim, seed=60)
         viser = LatentSpaceSaver_CondDirectTask(
             model_container=model_container,
             data_gen=data_gen2,
             fit_samples_num=4096,
             save_data_dir="/mnt/JupyterNotebook/interactive_latent_exploration/data",
             df_save_fn="LatentSpace_FitByEqual_Cond-Direct-Task.pickle",
-            vid_dirname="",
+            vid_dirname="Cond-Direct-Task",
             model_identifier=model_identifier,
-            draw=False
+            draw=True
         )
         viser.process()
 
-        # save_for_convex_direction(model_container=model_container,
-        #                           data_gen=data_gen2,
-        #                           fit_samples_num=4096,
-        #                           save_data_dir="/mnt/JupyterNotebook/interactive_latent_exploration/data",
-        #                           model_identifier=model_identifier,
-        #                           draw=False)
 
-    # else:
-    #     print("Chkpt cannot be found")
+    else:
+        print("Chkpt cannot be found")
+
+def dual_fingerprint_analysis():
+    from Spatiotemporal_VAE.analysis_scripts.fingerprint import DualDirectTaskFingerprintSaver
+    df_path = "/mnt/data/feas_tasks_phenos_nanMasks_idpatient_leg.pickle"
+    model_identifier_direct = "CB-K(0.0001)-C-G-S2-New"  # Direction
+    model_identifier_directtask = "Cond_Task_Direct_K-0.0001"  # Direction + Task
+
+    model_container_direct, _ = load_model_container(model_identifier_direct, df_path)
+    model_container_directtask, _ = load_model_container(model_identifier_directtask, df_path)
+    saver = DualDirectTaskFingerprintSaver(model_container1=model_container_direct,
+                                           model_container2=model_container_directtask,
+                                           load_df_path=df_path,
+                                           save_data_dir="/mnt/JupyterNotebook/interactive_latent_exploration/data",
+                                           save_df_name="Fingerprint_dual_direct_task.pickle")
+    saver.process_and_save()
+
+def single_fingerprint_analysis():
+    from Spatiotemporal_VAE.analysis_scripts.fingerprint import CondFingerprintSaver
+    df_path = "/mnt/data/feas_tasks_phenos_nanMasks_idpatient_leg.pickle"
+    model_identifier_direct = "CB-K(0.0001)-C-G-S2-New"  # Direction
+    model_container_direct, _ = load_model_container(model_identifier_direct, df_path)
+    saver = CondFingerprintSaver(model_container=model_container_direct,
+                                 load_df_path=df_path,
+                                 save_data_dir="/mnt/JupyterNotebook/interactive_latent_exploration/data",
+                                 save_df_name="Fingerprint_direct.pickle")
+    saver.process_and_save()
